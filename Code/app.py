cdf1480140615be88b7cae0f61ae71a735d97b85
Teije Hidde Donker <teijehidde@gmail.com>
2021-03-29 15:28:13 +0100

    retreive links from one page through wikimedia api

--- Code/app.py
+++ Code/app.py
@@ -16,7 +16,9 @@ import requests
 
 WIKI_URL = "https://en.wikipedia.org"
 API_ENDPOINT = WIKI_URL + "/w/api.php"
+# pagename = "Albert Einstein"
 
+"""
 # App config.
 DEBUG = True
 APP = Flask(__name__)
@@ -25,32 +27,38 @@ APP.config['SECRET_KEY'] = 'enter_your_secret_key'
 
 @APP.route("/", methods=['GET'])
 def index():
-    """ Displays the index page accessible at '/'
-    """
-    username = request.args.get('username')
+    pagename = request.args.get('pagename')
 
-    if username is not None:
-        data = get_user_contribs(username)
+    if pagename is not None:
+        data = get_user_contribs(pagename)
     else:
         data = None
 
-    return render_template('user_contributions.html', data=data, \
-        username=username, wikiurl=WIKI_URL)
+    return render_template('page_links.html', data=data, \
+        pagename=pagename, wikiurl=WIKI_URL)
+"""
 
-def get_user_contribs(username):
-    """ Fetch user contributions via MediaWiki API's Usercontribs module """
+def get_page_links(pagetitle):
+    """ Fetch links via MediaWiki API's links module """
     params = {
         "action": "query",
         "format": "json",
-        "list": "usercontribs",
-        "ucprop": "user|ids|title|sizediff",
-        "ucuser": username,
-        "uclimit": 50
+        "titles": pagetitle,
+        "prop": "links"
     }
 
     response = requests.get(url=API_ENDPOINT, params=params)
     data = response.json()
-    return data['query']['usercontribs']
+    page = next(iter(data['query']['pages']))
+    return data['query']['pages'][page]
 
+
+result = get_page_links("Alexander Friedmann")
+print(result['links'])
+# print(type(result['prop']))
+
+# print(data['links'])
+"""
 if __name__ == '__main__':
     APP.run()
+"""
