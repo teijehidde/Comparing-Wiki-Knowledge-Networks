6ec79d1c953a94a5a4b0e5eb5a972cf2ce89f569
Teije Hidde Donker <teijehidde@gmail.com>
2021-08-09 20:46:25 +0100

    CWKN-85 Created a (very basic) command line tool for overview of networks. Downloading networks WIP, but almost done.

--- Code/fetch_data.py
+++ Code/fetch_data.py
@@ -1,212 +1,185 @@
-#!/usr/bin/python3
-
-"""
-    MediaWiki API Demos
-
-    This command line tool fetches data from Wikimedia API and saves json files that are directly usable in drawing networks using PyGraphviz and networkx packages.
-    Actual drawing is done in app.py (that draws purely on json file) 
-
-    MIT license
-"""
-
-#Setup 
-import requests
-from flask import Flask, render_template, request
-import json
+# Setup packages and file paths 
 import os
-from time import sleep
-from datetime import date
+from itertools import chain
+from collections import Counter
+import collections 
+import requests
+import json
+import pandas as pd
+import numpy as np 
+import zipfile
+from io import BytesIO
 
-PATH = '/home/teijehidde/Documents/Git Blog and Coding/Project one (wikipedia SNA)/Code/'
-DATA_FILE = 'networkdata.json'
-WIKI_URL = "https://en.wikipedia.org"
-API_ENDPOINT = WIKI_URL + "/w/api.php"
-LIMIT_LINKS_PER_NODE = 500
-LIMIT_API_REQUESTS = 100
-
+path = "/home/teijehidde/Documents/Git Blog and Coding/data_dump/"
+data_file = "data_new2.json"
 
 # Check if json file "DATA_FILE" is present in folder. 
 def WikiNetworkDataMAIN():
-    
     os.system('clear')
 
     try:
-        with open(PATH + DATA_FILE) as json_file:
-            global network_data
-            network_data = json.load(json_file)
-    except IOError:
-        print("Error: Could not find " + DATA_FILE + ". Please check if file is present in directory, or change DATA_FILE value.")
-    else: 
-        print("The file " + DATA_FILE + " found, succesfully loaded.")
-        print("Network data accessed through " + API_ENDPOINT + ".")
+        pd.read_json((path + data_file), orient='split')
+        print("The file " + data_file + " found.")
         SelectMenu()
+    except IOError:
+        print("Error: Could not find " + data_file + ". Please check if file is present in directory, or change data_file value.")
 
-# Select function to be run on json file.  
+# Function A: the actual function that calls the wikipedia API and outputs a panda table with curated date. 
+def downloadWikiNetwork(node_title, lang = "en"):
+
+    api_endpoint = "https://" + lang + ".wikipedia.org/w/api.php" # fr.wikipedia.org; https://en.wikipedia.org
+    wiki_data = []
+    print("Starting download of network " + node_title + " in language " + lang + ".")
+    
+    # 1: download data on the node_title wikipage.
+    S = requests.Session()
+    params_node_title = {
+        "action": "query",
+        "titles": node_title,
+        "prop": "info|links|langlinks", 
+        "plnamespace": 0, 
+        "pllimit": 500,
+        "lllimit": 500, 
+        "format": "json"
+    }
+    response = S.get(url=api_endpoint, params=params_node_title)
+    wiki_data.append(response.json())
+    
+    while 'continue' in wiki_data[-1].keys():
+        params_cont = params_node_title
+        if 'plcontinue' in wiki_data[-1]['continue']:
+            params_cont["plcontinue"] = wiki_data[-1]['continue']['plcontinue'] 
+            print('plcontinue: ' + params_cont["plcontinue"])
+
+        elif 'llcontinue' in wiki_data[-1]['continue']:
+            params_cont["llcontinue"] = wiki_data[-1]['continue']['llcontinue'] 
+            print('llcontinue: ' + params_cont["llcontinue"])
+
+        response = S.get(url=api_endpoint, params=params_cont)
+        wiki_data.append(response.json())
+
+    # 2: download data on the links of node_title wikipage.
+    S = requests.Session()
+    params_network_title = {
+        "action": "query",
+        "generator": "links",
+        "titles": node_title,
+        "gplnamespace": 0, 
+        "gpllimit": 500, 
+        "plnamespace": 0,
+        "pllimit": 500, 
+        "prop": "info|links", 
+        "format": "json"
+    }
+    response = S.get(url=api_endpoint, params=params_network_title)
+    wiki_data.append(response.json())
+
+    while 'continue' in wiki_data[-1].keys():
+        params_cont = params_network_title
+        if 'plcontinue' in wiki_data[-1]['continue']:
+            params_cont["plcontinue"] = wiki_data[-1]['continue']['plcontinue'] 
+            print('plcontinue: ' + params_cont["plcontinue"])
+
+        elif 'gplcontinue' in wiki_data[-1]['continue']: 
+            params_cont["gplcontinue"] = wiki_data[-1]['continue']['gplcontinue']
+            print('gplcontinue: ' + params_cont["gplcontinue"])
+
+        response = S.get(url=api_endpoint, params = params_cont)
+        wiki_data.append(response.json())
+
+    # 3: creating list of available nodes in wiki_data. 
+    all_nodes = []
+
+    for item in wiki_data:
+        all_nodes = all_nodes + list(item['query']['pages'].keys())
+    all_nodes = list(set(all_nodes))
+    all_nodes = [int(i) for i in all_nodes if int(i) > 0]
+    all_nodes = [str(i) for i in all_nodes]
+    
+    network_data_df = pd.DataFrame(
+        columns = ['title', 'lang', 'pageid', 'uniqueid', 'lastrevid', 'links', 'langlinks'], # complete list: ['ns', 'title', 'missing', 'contentmodel', 'pagelanguage', 'pagelanguagehtmlcode', 'pageid', 'lastrevid', 'length', 'links', 'langlinks']
+        index = all_nodes)
+
+    # 4: Using all_nodes to go through list of raw data from API. 
+    for node in all_nodes:
+                
+        for item in wiki_data:
+            if node in item['query']['pages'].keys(): # possibility:  df_new_wiki_data.update(item) #, errors = 'raise') 
+                network_data_df.at[node, 'title'] = item['query']['pages'][node]['title']
+                network_data_df.at[node,'lang'] = item['query']['pages'][node]['pagelanguage']
+                network_data_df.at[node,'pageid'] = item['query']['pages'][node]['pageid']
+                network_data_df.at[node,'uniqueid'] = network_data_df.at[node,'lang'] + str(network_data_df.at[node,'pageid'])
+                network_data_df.at[node,'lastrevid'] = item['query']['pages'][node]['lastrevid']
+
+                if 'links' in item['query']['pages'][node].keys():
+                    links_temp = []
+                    for link in item['query']['pages'][node]['links']:
+                        links_temp.append(link['title'])
+                    network_data_df.at[node,'links'] = links_temp
+
+                if 'langlinks' in item['query']['pages'][node].keys():
+                    network_data_df.at[node,'langlinks'] = item['query']['pages'][node]['langlinks']
+
+    # returns panda with all data from network. 
+    return network_data_df
+
+# Function B: Downloading multiple languages and saving them to json/panda file. 
+def downloadMultiLangWikiNetwork(node_title, original_lang = 'en', additional_langs = ["ar", "de", "fr", "nl"]): # or: 'available_langs'
+    network_data_df = downloadWikiNetwork(node_title=node_title, lang=original_lang)
+    available_langs = network_data_df.loc[network_data_df['title'] == node_title].loc[network_data_df['lang'] == original_lang]['langlinks'].values.tolist()[0]
+
+    if additional_langs == []:
+        print('The wikipedia page is available in the following languages:')         
+        print(available_langs)
+    
+    else:
+        for item in available_langs: 
+            if item['lang'] in additional_langs:
+                network_data_df_additional = downloadWikiNetwork(node_title = item['*'], lang = item['lang'])
+                network_data_df = pd.concat([network_data_df, network_data_df_additional], ignore_index=True, sort=False)
+                
+    network_data_saved = pd.read_json((path + data_file), orient='split')
+    network_data_df = pd.concat([network_data_df, network_data_saved], ignore_index=True, sort=False)
+    network_data_df = network_data_df.loc[network_data_df.astype(str).drop_duplicates(keep = 'last').index].reset_index(drop=True)
+    network_data_df.to_json((path + data_file), orient='split')
+    print("Download of network and additional languages finished.") 
+
+
+# Function 0: Select function to be run on json file.  
 def SelectMenu():
 
     print("   ")
     print("---")
     print("   ")
-    print("This is a command line tool to build, append and update " + DATA_FILE + ". Please select one of the following options:")
+    print("This is a command line tool to build, append and update " + data_file + ". Please select one of the following options:")
     print("   ")
-    print("1 = List existing networks in " + DATA_FILE + ".")
-    print("2 = Check status of an existing network.")
-    print("3 = Build a new network.")
-    print("4 = Append existing network.")
-    print("5 = Update existing network.")
-    print("6 = Update revision history of existing network.")
-    print("9 = Save json file and exit program.")
+    print("1 = Overview available networks in " + data_file + ".") # should output panda datframe: index = topics; columns = languages available; cells = number of nodes. 
+    print("2 = Download and save new network.")
 
-    choice_int = int(input("Please type a number between 1 and 9 to select one of the above options:" ))
+    choice_int = int(input("Please type a number between 1 and 2 to select one of the above options:" ))
     
-    if choice_int == 1: ListNetworks()
-    if choice_int == 2: CheckStatusNetwork()
-    if choice_int == 3: BuildNetwork()
-    if choice_int == 4: AppendNetwork()
-    if choice_int == 5: UpdateNetwork()
-    if choice_int == 6: UpdateRevisionsNetwork()
-    if choice_int == 9: SaveAndExit() 
+    if choice_int == 1: overviewNetworks()
+    if choice_int == 2: downloadNetworks()
     else: 
-        os.system('clear')
+        # os.system('clear')
         print("Please type a number.")
         print("   ")
         SelectMenu()
 
-# Function 0: Download Node.
-def DownloadNode(node_title):
-    
-    # TODO: error handling if page is not present on wikipedia. 
-    # setup 
-    edges = []
+# Function 1: provide overview of available networks. 
+def overviewNetworks():
+    network_data_df = pd.read_json((path + data_file), orient='split')
+    available_wiki_networks = network_data_df.loc[network_data_df['langlinks'].notnull()]
+    available_wiki_networks['number_of_links'] = available_wiki_networks['links'].apply(lambda x: len(x)) # here it throws an error. FIX. 
 
-    # requesting data via wikimedia API.  
-    S = requests.Session()
-    PARAMS = {
-        "action": "query",
-        "format": "json",
-        "titles": node_title,
-        "prop": "links",
-        # "plcontinue": ,
-        "plnamespace": 0, # only load wikipedia main/articles. 
-        "pllimit": 'max' # can go up to 500. Go for max? 
-    }
+    print(available_wiki_networks[['title', 'lang', 'pageid', 'number_of_links']])
 
-   
-    response = S.get(url=API_ENDPOINT, params=PARAMS)
-    
-    # Transforming response to network data format.  
-    data_wiki = response.json()
-    node = next(iter(data_wiki['query']['pages']))
-    
-    try: 
-        for x in data_wiki['query']['pages'][node]['links']:
-            edges.append(x['title'])
-            
-        node_data = {'node_ID': node, 'ego': 0, 'date_time': 'TODO', 'edges': edges, 'revisions': 'TODO'}
-        network_data[node_title] = node_data
-    except:
-        print("An exception occured while accessing " + node_title + ".")
-    else: 
-        print("Links data on page " + node_title + " successfully downloaded.")
+# Function 2: request name of network + languages and download networks in these languages .  
+def downloadNetworks():
+    print('to be implemnted')
 
-        with open(PATH + DATA_FILE, 'w') as outfile:
-            json.dump(network_data, outfile)
-            print("Data succesfully saved.")
-
-    # sleep(0.5)
-
-# Function 1: Summary data in Json file. 
-def ListNetworks():
-    os.system('clear')
-
-    print("Currently there are " + str(len(network_data.keys())) + " nodes in " + DATA_FILE + ".")
-    print("  ")
-    
-    print("Nodes with ego status are: ")
-    
-    for item in network_data.keys():
-        if network_data[item]['ego'] == 1:
-            print(item)
-
-    sleep(1)
-    SelectMenu()
-    
-# Function 2: Get summary of available data on one network. WIP!!  
-def CheckStatusNetwork():
-    os.system('clear')
-
-    pagetitle = input("Please provide a title included in " + DATA_FILE + ": ")
-    
-    print('Number of nodes in level:')
-    for item in network_data[pagetitle]['edges']:
-        print(item)
-    
-    sleep(1)
-    SelectMenu()
-
-# Function 3: Build new network - first layer only. WORKS! (but very prelim, no checks, error handling etc what so ever. WIP.)
-def BuildNetwork():
-
-    # resquesting name of node to download.
-    node_title = input("Please provide a title of a Wikipedia page: ")
-
-    DownloadNode(node_title)
-    network_data[node_title]['ego'] = 1
-
-# Function 4: Append one level (or remainder of one level) to existing network. Includes a throttle.  TODO
-def AppendNetwork():
-    os.system('clear')
-    
-    node_title = input("Please provide a the title of a node included in " + DATA_FILE + ": ")
-
-    if node_title in network_data:
-
-       counter = 0       
-       node_requests = set(network_data[node_title]['edges']).difference(network_data.keys())
-
-       if node_requests == None: 
-           print("All nodes have been downloaded.")
-       else: 
-           print("Link data from maximum of " + str(LIMIT_API_REQUESTS) + " pages will be downloaded.")
-           print("   ")
-       
-           for item in node_requests:
-               DownloadNode(item)
-               counter = counter + 1
-               print(counter)
-               if counter == LIMIT_API_REQUESTS: break            
-    
-    else: 
-        print("Title not present in " + DATA_FILE + ".")
-
-    # sleep(.1)
-    SelectMenu()
-
-# Function 5: Checks for changes since last action on network and updates. TODO
-def UpdateNetwork():
-    os.system('clear')
-    print("UpdateNetwork is not implemented yet. Returning to main menu.")
-    sleep(1)
-    SelectMenu()
-
-# Function 6: Creates a revision history of complete network. Enables changing network over time. Includes a throttle. TODO
-def UpdateRevisionsNetwork():
-    os.system('clear')
-    print("UpdateRevisionsNetwork is not implemented yet. Returning to main menu.")
-    sleep(1)
-    SelectMenu()
-
-def SaveAndExit():
-    # Dumping data into file and saving. DONE 
-
-    with open(PATH + DATA_FILE, 'w') as outfile:
-        json.dump(network_data, outfile)
-        print("Data succesfully saved. Exiting program.")
-        exit()
-    
-
-    
-# NB: RUNTIME
-WikiNetworkDataMAIN()
+# NB: RUNTIME -- this should be written differently.
+if __name__ == '__main__':
+    WikiNetworkDataMAIN()
 
 # END 
